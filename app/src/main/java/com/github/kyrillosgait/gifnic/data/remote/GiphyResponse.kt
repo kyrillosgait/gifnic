9cb33016afc66186238c5cffd54616fabf5c199a
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2019-09-15 16:01:50 +0300

    add Giphy response model

---
+++ app/src/main/java/com/github/kyrillosgait/gifnic/data/remote/GiphyResponse.kt
@@ -0,0 +1,15 @@
+package com.github.kyrillosgait.gifnic.data.remote
+
+import com.github.kyrillosgait.gifnic.data.models.Meta
+import com.github.kyrillosgait.gifnic.data.models.Pagination
+import kotlinx.serialization.Serializable
+
+/**
+ * Successful response from GIPHY API.
+ */
+@Serializable
+data class GiphyResponse<T>(
+    val data: T? = null,
+    val pagination: Pagination? = null,
+    val meta: Meta? = null
+)
