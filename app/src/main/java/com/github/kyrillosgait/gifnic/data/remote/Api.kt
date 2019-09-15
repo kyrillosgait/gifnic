0ba38d68b98d53f74a80aed23119809726aa85ea
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2019-09-15 16:05:03 +0300

    set up Retrofit interface with Trending and Random api calls

---
+++ app/src/main/java/com/github/kyrillosgait/gifnic/data/remote/Api.kt
@@ -0,0 +1,58 @@
+package com.github.kyrillosgait.gifnic.data.remote
+
+import com.github.kyrillosgait.gifnic.data.models.Gif
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+;
+
+const val BASE_URl = "https://api.giphy.com/v1/"
+
+private const val GIFS_TRENDING = "gifs/trending"
+private const val GIFS_RANDOM = "gifs/random"
+
+private const val API_KEY = "dc6zaTOxFJmzC"
+
+/**
+ * Interface to define REST API calls.
+ */
+interface Api {
+
+    /**
+     * Returns a list of the most relevant and engaging content each and every day.
+     *
+     * @param apiKey giphy API key.
+     * @param limit the maximum number of objects to return.
+     * @param offset specifies the starting position of the results.
+     * @param rating filters results by specified rating.
+     * @param randomId an ID/proxy for a specific user.
+     *
+     * @return a list of trending GIFs
+     */
+    @GET(GIFS_TRENDING)
+    suspend fun getTrending(
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("limit") limit: String? = "20",
+        @Query("offset") offset: String? = null,
+        @Query("rating") rating: String? = null,
+        @Query("random_id") randomId: String? = null
+    ): Response<GiphyResponse<List<Gif>>>
+
+    /**
+     * Returns a single random GIF.
+     *
+     * @param apiKey giphy API key.
+     * @param tag filters results by specified tag.
+     * @param rating filters results by specified rating.
+     * @param randomId an ID/proxy for a specific user.
+     *
+     * @return a random GIF.
+     */
+    @GET(GIFS_RANDOM)
+    suspend fun getRandom(
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("tag") tag: String? = null,
+        @Query("rating") rating: String? = null,
+        @Query("random_id") randomId: String? = null
+    ): Response<GiphyResponse<Gif>>
+}
