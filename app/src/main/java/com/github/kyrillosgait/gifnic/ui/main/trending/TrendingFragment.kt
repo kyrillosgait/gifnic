6234fa992b073dc4de001e21b119b54c141ca207
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2019-09-15 16:25:47 +0300

    add trending fragment
    - use mainViewModel to get the gifs PagedList and submit it to the adapter,
    - add dark theme toggle
    - initialize recycler view and correctly manage the state of the list

---
+++ app/src/main/java/com/github/kyrillosgait/gifnic/ui/main/trending/TrendingFragment.kt
@@ -0,0 +1,108 @@
+package com.github.kyrillosgait.gifnic.ui.main.trending
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.github.kyrillosgait.gifnic.R
+import com.github.kyrillosgait.gifnic.data.models.Gif
+import com.github.kyrillosgait.gifnic.di.activityViewModel
+import com.github.kyrillosgait.gifnic.ui.common.State
+import com.github.kyrillosgait.gifnic.ui.common.gone
+import com.github.kyrillosgait.gifnic.ui.common.onClick
+import com.github.kyrillosgait.gifnic.ui.common.visible
+import kotlinx.android.synthetic.main.fragment_trending.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TrendingFragment : Fragment(R.layout.fragment_trending) {
+
+    // region Properties
+
+    private val activityViewModel by activityViewModel { mainViewModel }
+
+    private lateinit var gifsAdapter: GifsAdapter
+    private lateinit var gifsLayoutManager: GridLayoutManager
+
+    // endregion
+
+    // region Lifecycle
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        initToolbar()
+        initRecyclerView()
+    }
+
+    private fun initToolbar() {
+        trendingNightModeToggleIcon.onClick {
+            AppCompatDelegate.setDefaultNightMode(
+                when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                    Configuration.UI_MODE_NIGHT_NO -> AppCompatDelegate.MODE_NIGHT_YES
+                    else -> AppCompatDelegate.MODE_NIGHT_NO
+                }
+            ).also { activity?.recreate() }
+        }
+    }
+
+    // endregion
+
+    private fun initRecyclerView() {
+        val testLayoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+
+        gifsLayoutManager = GridLayoutManager(requireContext(), 2)
+
+        val onGifClicked: (Gif) -> Unit = {
+            findNavController().navigate(
+                TrendingFragmentDirections.actionTrendingFragmentToDetailFragment(gif = it)
+            )
+        }
+
+        val onGifLongClicked: (Gif) -> Unit = {
+            findNavController().navigate(
+                TrendingFragmentDirections.actionTrendingFragmentToPreviewDialogFragment(gif = it)
+            )
+        }
+
+        gifsAdapter = GifsAdapter(
+            { onGifClicked(it) },
+            { onGifLongClicked(it) }
+        )
+
+        gifsRecyclerView.apply {
+            adapter = gifsAdapter
+            layoutManager = testLayoutManager
+            itemAnimator = null
+        }
+
+        activityViewModel.gifs.observe {
+            when (it) {
+                is State.Loading -> {
+                    progressBar.visible()
+                    errorIcon.gone()
+                }
+                is State.Success -> {
+                    gifsAdapter.submitList(it.data)
+                    progressBar.gone()
+                    errorIcon.gone()
+                }
+                is State.Empty -> Unit
+                is State.Error -> {
+                    errorIcon.visible()
+                    progressBar.gone()
+                }
+            }
+        }
+    }
+
+    private fun <T> LiveData<T>.observe(consumer: (T) -> Unit) {
+        observe(this@TrendingFragment, Observer { consumer(it) })
+    }
+}
