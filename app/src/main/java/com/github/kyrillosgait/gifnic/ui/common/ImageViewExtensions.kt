87b8336cd5b5f09619460116bdf7e94d02041998
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2019-09-15 16:27:55 +0300

    add ImageViewExtensions to test multiple image loading methods

---
+++ app/src/main/java/com/github/kyrillosgait/gifnic/ui/common/ImageViewExtensions.kt
@@ -0,0 +1,42 @@
+package com.github.kyrillosgait.gifnic.ui.common
+
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+
+/**
+ * Loads an image from a url into the [ImageView].
+ */
+fun ImageView.loadGif(
+    url: String,
+    diskCacheStrategy: DiskCacheStrategy = DiskCacheStrategy.AUTOMATIC
+) {
+    Glide.with(context)
+        .asGif()
+        .load(url)
+        .diskCacheStrategy(diskCacheStrategy)
+        .into(this)
+}
+
+/**
+ * Loads an image from a url into the [ImageView].
+ */
+fun ImageView.loadWebp(
+    url: String,
+    diskCacheStrategy: DiskCacheStrategy = DiskCacheStrategy.AUTOMATIC
+) {
+    Glide.with(context)
+        .load(url)
+        .into(this)
+}
+
+fun ImageView.loadAnimatedGifWithPlaceholder(
+    animatedUrl: String,
+    staticUrl: String
+) {
+    Glide.with(context)
+        .asGif()
+        .thumbnail(Glide.with(context).asGif().load(staticUrl))
+        .load(animatedUrl)
+        .into(this)
+}
