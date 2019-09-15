ada1e63a77f9124936c2f8c88cd3b06c9bbb7bee
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2019-09-16 01:06:20 +0300

    remove unused properties from the Image object

--- app/src/main/java/com/github/kyrillosgait/gifnic/data/models/Image.kt
+++ app/src/main/java/com/github/kyrillosgait/gifnic/data/models/Image.kt
@@ -2,7 +2,6 @@ package com.github.kyrillosgait.gifnic.data.models
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable @Parcelize
@@ -11,8 +10,5 @@ data class Image(
     val width: String = "",
     val height: String = "",
     val size: String = "",
-    val mp4: String = "",
-    val webp: String = "",
-    @SerialName("mp4_size") val mp4Size: String = "",
-    @SerialName("webp_size") val webp_size: String = ""
+    val webp: String = ""
 ) : Parcelable
