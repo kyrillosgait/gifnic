ecf93d490c46791083525ecaf37d1446d3ead34e
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2019-09-17 21:57:20 +0300

    fix navigation bar icon color in dark theme

--- app/src/main/java/com/github/kyrillosgait/gifnic/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/kyrillosgait/gifnic/ui/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.github.kyrillosgait.gifnic.ui.main
 
+import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -13,23 +14,30 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         Navigation.findNavController(this, R.id.mainNavHostFragment)
     }
 
-    private val drawBehindSystemBars: () -> Unit = {
-        when {
-            Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> {
-                window.decorView.systemUiVisibility =
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                            View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-            }
-            else -> {
-                window.decorView.systemUiVisibility =
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            }
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val isDarkThemeEnabled: () -> Boolean = {
+            when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_YES -> true
+                else -> false
+            }
+        }
+
+        val drawBehindSystemBars: () -> Unit = {
+            when {
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !isDarkThemeEnabled() -> {
+                    window.decorView.systemUiVisibility =
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                                View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                }
+                else -> {
+                    window.decorView.systemUiVisibility =
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                }
+            }
+        }
 
         drawBehindSystemBars()
     }
