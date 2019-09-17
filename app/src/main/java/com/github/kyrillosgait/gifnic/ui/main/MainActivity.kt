068b1ac9469e21c99ad88b4369905038096d09e6
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2019-09-17 19:58:08 +0300

    enable main activity to draw content behind the systems bars (status bar, nagivation bar)

--- app/src/main/java/com/github/kyrillosgait/gifnic/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/kyrillosgait/gifnic/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.kyrillosgait.gifnic.ui.main
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import com.github.kyrillosgait.gifnic.R
@@ -13,6 +14,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         Navigation.findNavController(this, R.id.mainNavHostFragment)
     }
 
+    private val drawBehindSystemBars: () -> Unit = {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,6 +26,8 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             Configuration.UI_MODE_NIGHT_NO -> setLightStatusBar()
             else -> Unit
         }
+
+        drawBehindSystemBars()
     }
 
     override fun onSupportNavigateUp(): Boolean = navController.navigateUp()
