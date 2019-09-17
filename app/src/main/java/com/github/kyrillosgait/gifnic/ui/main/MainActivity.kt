3d390526e3c82637936692cebc84e1b98a8a0851
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2019-09-17 21:33:43 +0300

    use light navigation bar when running on oreo and above

--- app/src/main/java/com/github/kyrillosgait/gifnic/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/kyrillosgait/gifnic/ui/main/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.github.kyrillosgait.gifnic.ui.main
 
-import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import com.github.kyrillosgait.gifnic.R
-import com.github.kyrillosgait.gifnic.ui.common.setLightStatusBar
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -15,17 +14,22 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     }
 
     private val drawBehindSystemBars: () -> Unit = {
-        window.decorView.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.O -> {
+                window.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            }
+            else -> {
+                window.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-            Configuration.UI_MODE_NIGHT_NO -> setLightStatusBar()
-            else -> Unit
-        }
 
         drawBehindSystemBars()
     }
