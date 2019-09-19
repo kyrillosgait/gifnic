b85035f947bda42d87b991568dd61e6b5e8b5eb2
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2019-09-19 17:36:45 +0300

    improvements to ImageView extensions

--- app/src/main/java/com/github/kyrillosgait/gifnic/ui/common/ImageViewExtensions.kt
+++ app/src/main/java/com/github/kyrillosgait/gifnic/ui/common/ImageViewExtensions.kt
@@ -5,21 +5,10 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 /**
- * Loads a GIF image from a url into the [ImageView].
- */
-fun ImageView.loadGif(
-    url: String,
-    diskCacheStrategy: DiskCacheStrategy = DiskCacheStrategy.AUTOMATIC
-) {
-    Glide.with(context)
-        .asGif()
-        .load(url)
-        .diskCacheStrategy(diskCacheStrategy)
-        .into(this)
-}
-
-/**
  * Loads a WEBP image from a url into the [ImageView].
+ *
+ * @param url the url of the image to load.
+ * @param diskCacheStrategy the caching strategy.
  */
 fun ImageView.loadWebp(
     url: String,
@@ -31,13 +20,21 @@ fun ImageView.loadWebp(
         .into(this)
 }
 
-fun ImageView.loadAnimatedGifWithPlaceholder(
-    animatedUrl: String,
-    staticUrl: String
+/**
+ * Loads a WEBP image from a url into the [ImageView], with a placeholder.
+ *
+ * @param url the url of the image to load.
+ * @param placeholder the url of the placeholder image to load before the main image.
+ * @param diskCacheStrategy the caching strategy.
+ */
+fun ImageView.loadWebpWithPlaceholder(
+    url: String,
+    placeholder: String,
+    diskCacheStrategy: DiskCacheStrategy = DiskCacheStrategy.AUTOMATIC
 ) {
     Glide.with(context)
-        .asGif()
-        .thumbnail(Glide.with(context).asGif().load(staticUrl))
-        .load(animatedUrl)
+        .load(url)
+        .thumbnail(Glide.with(context).asDrawable().load(placeholder))
+        .diskCacheStrategy(diskCacheStrategy)
         .into(this)
 }
