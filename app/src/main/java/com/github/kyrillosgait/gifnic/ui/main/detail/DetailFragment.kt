9cc20cd4c4ed428de36b457a080fde9c722fc79d
Kyrillos Gaitanis <kyrillos.gait@gmail.com>
2019-09-19 17:37:42 +0300

    improvements in detail fragment and fix GIFs with no title

--- app/src/main/java/com/github/kyrillosgait/gifnic/ui/main/detail/DetailFragment.kt
+++ app/src/main/java/com/github/kyrillosgait/gifnic/ui/main/detail/DetailFragment.kt
@@ -6,9 +6,12 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.github.kyrillosgait.gifnic.R
+import com.github.kyrillosgait.gifnic.data.models.Gif
 import com.github.kyrillosgait.gifnic.di.viewModel
 import com.github.kyrillosgait.gifnic.ui.common.State
+import com.github.kyrillosgait.gifnic.ui.common.gone
 import com.github.kyrillosgait.gifnic.ui.common.loadWebp
+import com.github.kyrillosgait.gifnic.ui.common.visible
 import kotlinx.android.synthetic.main.fragment_detail.*
 
 /**
@@ -21,32 +24,34 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
     private val safeArgs: DetailFragmentArgs by navArgs()
     private val viewModel by viewModel { detailViewModel }
 
+    private val getGifTitle: (Gif) -> String = { gif ->
+        when {
+            gif.title.isNotBlank() -> gif.title
+            else -> "untitled GIF"
+        }
+    }
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        initToolbar()
-        initImage()
-    }
+        detailToolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
 
-    private fun initToolbar() {
-        detailToolbar.setNavigationOnClickListener { findNavController().navigateUp() }
-        detailToolbar.title = safeArgs.gif.title
-    }
-
-    private fun initImage() {
-        detailAnimatedImageView.loadWebp(url = safeArgs.gif.images.fixedWidth.webp)
+        detailToolbar.title = getGifTitle(safeArgs.gif)
+        detailImageView.loadWebp(url = safeArgs.gif.images.fixedWidth.webp)
 
         viewModel.randomGif.observe(this, Observer {
             when (it) {
-                is State.Loading -> Unit
-                is State.Empty -> Unit
+                is State.Loading -> detailProgressBar.visible()
+                is State.Empty -> detailProgressBar.gone()
                 is State.Success -> {
-                    detailToolbar.title = it.data.title
-                    detailAnimatedImageView.loadWebp(url = it.data.images.fixedWidth.webp)
+                    detailToolbar.title = getGifTitle(it.data)
+                    detailImageView.loadWebp(url = it.data.images.fixedWidth.webp)
+                    detailProgressBar.gone()
                 }
-                is State.Error -> Unit
+                is State.Error -> detailProgressBar.gone()
             }
         })
     }
-
 }
